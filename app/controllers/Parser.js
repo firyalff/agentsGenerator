5c7d5eee89ebe3061b3a2d8b9fd3f4e7c5700d2d
firyalf <firyal.fakhril@gmail.com>
2017-04-25 14:57:47 +0700

    [ADD-FEATURE] multipart request handler works

--- app/controllers/Parser.js
+++ app/controllers/Parser.js
@@ -3,10 +3,39 @@
 const models  = require(__dirname+'/../models')
 , APIFormat  = require(__dirname+'/../helpers/APIFormat')
 , parse = require('csv-parse')
+, uploader  = require(__dirname+'/../helpers/MultipartRead')
 , methods = {
 
+	index(req, res) {
+		return res.json({'test': 'fafa'}); 
+	},
 	store(req, res) {
-		console.log(req)
+		var inputFields = [
+		{
+			name: 'test', 
+			maxCount:1
+		}, 
+		];
+
+		let promiseUpload = new Promise((resolve, reject) => {
+			uploader.upload(inputFields)(req, res, function(err) {
+				if (err) {
+					reject(err);
+				}
+				else {
+					resolve('product image uploaded');
+				}
+			})
+		});
+
+
+		return promiseUpload
+		.then(values => {
+			console.log(req.files)
+			
+			return res.json({'test': 'fafa'});
+		})
+
 	}
 }
 
