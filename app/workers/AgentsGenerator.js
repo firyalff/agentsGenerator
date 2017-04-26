0a3d8b87a0ee9b99e4babfcd66ba57a38f175b7b
firyalf <firyal.fakhril@gmail.com>
2017-04-26 20:21:13 +0700

    [ADD-FEATURE] example of cron task

--- app/workers/AgentsGenerator.js
+++ app/workers/AgentsGenerator.js
@@ -1,1 +1,16 @@
 const models  = require(__dirname+'/../models')
+, workers = [{
+	name : 'baseAttributePopulate',
+	pattern : '* * * * * *',
+	action : () =>{
+		console.log('nicely done!');
+	}
+}, {
+	name : 'test',
+	pattern : '* * * * * *',
+	action : () =>{
+		console.log('a test!');
+	}
+}]
+
+module.exports = workers;
